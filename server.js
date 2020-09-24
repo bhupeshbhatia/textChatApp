df424c52933f20bd86411cb15a0c46e56d0b8341
bhupeshbhatia <bhupesh.bhatia@gmail.com>
2020-09-23 21:27:14 -0400

    Updated server.js to show correct port

--- server.js
+++ server.js
@@ -5,25 +5,26 @@ var socket = require('socket.io');
 
 var app = express();
 
-var server = app.listen(3000, function(){
-    console.log('Listening to requests on 3000 port')
-})
+var server = app.listen(process.env.PORT || 3000, function(){
+    var port = server.address().port;
+    console.log('Listening to requests on port ' + port);
+});
 
 
 //static files
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 //Socket setup
 var io = socket(server);
 
 io.on('connection',function(socket){
-    console.log('Client is connected:',socket.id)
+    console.log('Client is connected:',socket.id);
     
     socket.on('chat',function(data){
         io.sockets.emit('chat',data);
     });
 
     socket.on('typing',function(data){
-        socket.broadcast.emit('typing',data)
+        socket.broadcast.emit('typing',data);
     });
 })
